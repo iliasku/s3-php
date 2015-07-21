957827169c0b474f9d4d6160d4e1cf03d7f26b36
ilias <tarkofski@gmail.com>
2015-07-21 14:59:08 +0000

    Create s3.php

---
+++ s3.php
@@ -0,0 +1,39 @@
+<?PHP
+
+function gs_getStringToSign($request_type, $expires, $uri) {
+   return "$request_type\n\n\n$expires\n$uri";
+}
+
+function gs_encodeSignature($s, $key) {
+    $s = utf8_encode($s);
+    $s = hash_hmac('sha1', $s, $key, true);
+    $s = base64_encode($s);
+    return urlencode($s);
+}
+ 
+function gs_prepareS3URL($file, $bucket) {
+ 
+  $awsKeyId = "accesskey"; 
+  $awsSecretKey = "secretkey"; 
+ 
+ 
+  $file = rawurlencode($file); 
+  $file = str_replace('%2F', '/', $file);
+  $path = $bucket .'/'. $file;
+ 
+  $expires = strtotime('+1 hour');
+ 
+  $stringToSign = gs_getStringToSign('GET', $expires, "/$path"); 
+  $signature = gs_encodeSignature($stringToSign, $awsSecretKey); 
+
+  $url = "http://$bucket.s3.amazonaws.com/$file";
+  $url .= '?AWSAccessKeyId='.$awsKeyId
+         .'&Expires='.$expires
+         .'&Signature='.$signature;
+         
+  return $url;
+}
+
+  $file = "win.jpg";
+  $bucket = "bucket";
+  $link = gs_prepareS3URL($file, $bucket);
